68310d8c2bb78aee55cc5b580cc3b47439b3e938
rezonmain <adiazgomez101@gmail.com>
2023-06-27 12:47:58 -0700

    feat(str): documeting, modified bound method

--- src/utils/Str.ts
+++ src/utils/Str.ts
@@ -1,5 +1,21 @@
+/**
+ * String utilities
+ */
 export class Str {
-  static bound(str: string, limit = 50) {
-    return str.length > limit ? str.slice(0, limit) + "..." : str;
+  /**
+   * Bound a string to a certain length
+   * if the string is longer than the limit (defualt is 50 chars),
+   * it will be truncated and an ellipsis will be appended by defualt
+   * @param str
+   * @param limit
+   * @param ellipsis
+   * @returns
+   */
+  static bound(str: string, limit = 50, ellipsis = true) {
+    return str.length > limit
+      ? str.slice(0, limit) + ellipsis
+        ? "..."
+        : ""
+      : str;
   }
 }
