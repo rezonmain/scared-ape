81a75713faf343c26d8cccd7bad6bdf8844c079b
rezonmain <adiazgomez101@gmail.com>
2023-06-27 12:50:21 -0700

    feat(booter): move booting logic to Booter

--- src/ScaredApe.ts
+++ src/ScaredApe.ts
@@ -1,14 +1,11 @@
 import type { DB } from "./services/db/DB.js";
 import { SQLiteDB } from "./services/db/SQLiteDB.js";
-import { Seeder } from "./services/Seeder.js";
-import { ScrapersHelper } from "./utils/ScrapersHelper.js";
-import { Logger } from "./utils/Logger.js";
-import type { IScraper } from "./models/Scraper.js";
 import { Cache } from "./services/cache/Cache.js";
 import { Scheduler } from "./services/scheduler/Scheduler.js";
 import { Api } from "./services/api/Api.js";
 import { Telegram } from "./services/notifier/Telegram/Telegram.js";
 import { Fetcher } from "./services/Fetcher.js";
+import { Booter } from "./utils/Booter.js";
 
 /**
  * The main app class
@@ -29,35 +26,9 @@ export class ScaredApe {
     this.api = new Api(this.db, this.scheduler, this.cache);
   }
 
-  private async runScraper(name: IScraper["name"]) {
-    const scraper = await ScrapersHelper.getScraperInstance(
-      name,
-      this.db,
-      this.notifier
-    );
-    await scraper.scrape();
-  }
-
-  /**
-   * Bootstrap the app
-   * - Connect to the database
-   * - Run migrations
-   * - Seed the database
-   * - Run all the active scrapers
-   */
-  async bootstrap() {
-    await this.db.connect();
-    await this.db.migrate();
-    const seeder = new Seeder(this.db);
-    await seeder.seed();
-    // Run all the active scrapers
-    Logger.log("🔄 [🦍App][run()] Starting active scrapers...");
-    const activeScrapers = await this.db.getActiveScrapers();
-    await Promise.all(
-      activeScrapers.map((scraper) => this.runScraper(scraper.name))
-    );
-    Logger.log("✅ [🦍App][run()] All active scrapers finished running.");
-    this.cache.flush();
+  async boot() {
+    const booter = new Booter(this.db, this.cache);
+    booter.boot();
   }
 
   /**
---
+++ src/utils/Booter.ts
@@ -0,0 +1,62 @@
+import type { DB } from "../services/db/DB.js";
+import type { Cache } from "../services/cache/Cache.js";
+import { Seeder } from "../services/Seeder.js";
+import { Logger } from "./Logger.js";
+import { ScrapersHelper } from "./ScrapersHelper.js";
+import type { IScraper } from "../models/Scraper.js";
+
+/**
+ * Automate the app bootrapping process
+ *
+ */
+export class Booter {
+  private environment: string;
+  constructor(private db: DB, private cache: Cache, environment?: string) {
+    this.environment = environment ?? process.env.NODE_ENV ?? "dev";
+  }
+
+  get env() {
+    return this.environment;
+  }
+
+  private async runScraper(name: IScraper["name"]) {
+    // Run scraper without a notfier so that it doesn't send notifications
+    const scraper = await ScrapersHelper.getScraperInstance(name, this.db);
+    await scraper.scrape();
+  }
+
+  private getMissingConfigValues(): Map<string, string> {
+    const missingConfigValues = new Map<string, string>();
+
+    return missingConfigValues;
+  }
+
+  private async configWizard() {}
+
+  /**
+   * Boot the app.
+   *
+   * Bootrspping scared ape includes:
+   * - Adding missing configuration values
+   * - Creating SQLite DB
+   * - Running migrations
+   * - Seeding database
+   * - Initial data scrape run
+   */
+  async boot() {
+    Logger.log("🔄 [👾Booter][boot()] Booting scared-ape...");
+    await this.configWizard();
+    await this.db.connect();
+    await this.db.migrate();
+    const seeder = new Seeder(this.db);
+    await seeder.seed();
+    // Run all the active scrapers
+    Logger.log("🔄 [👾Booter][boot()] Starting active scrapers...");
+    const activeScrapers = await this.db.getActiveScrapers();
+    await Promise.all(
+      activeScrapers.map((scraper) => this.runScraper(scraper.name))
+    );
+    Logger.log("✅ [👾Booter][boot()] All active scrapers finished running.");
+    this.cache.flush();
+  }
+}
