1988fbb46f806584506962c92e3297d214c4e14e
rezonmain <adiazgomez101@gmail.com>
2023-06-27 12:49:31 -0700

    feat(booter): update boot config var

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import config from "config";
 import { ScaredApe } from "./ScaredApe.js";
 
 const app = new ScaredApe();
-config.get("app.bootstrapOnStartup") ? await app.bootstrap() : null;
+config.get("app.bootOnStart") ? await app.boot() : null;
 app.run();
