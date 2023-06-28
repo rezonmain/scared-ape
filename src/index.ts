ed61ec37e7f5e901d82ba1b3f406e0a75d663e97
rezonmain <adiazgomez101@gmail.com>
2023-06-28 11:42:05 -0700

    feat(index): improve warning message

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import { ScaredApe } from "./ScaredApe.js";
 import { FileHelper } from "./utils/FileHelper.js";
 
 if (!(await FileHelper.exists(`config/local-${process.env.NODE_ENV}.json5`))) {
-  console.log("🚨 No config file found. Run boot script: pnpm boot");
+  const env = process.env.NODE_ENV ?? "dev";
+  console.log(
+    `🚨 No config file found. Run boot script: pnpm boot${
+      env === "dev" ? ":dev" : ""
+    }`
+  );
   process.exit(1);
 }
 const app = new ScaredApe();
