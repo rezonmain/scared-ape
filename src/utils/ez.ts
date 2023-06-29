1569f390dab74cda4ad9513324960dc48d264ea0
rezonmain <adiazgomez101@gmail.com>
2023-06-28 21:49:48 -0700

    feat(ez): add unsafe coerce method

--- src/utils/ez.ts
+++ src/utils/ez.ts
@@ -25,12 +25,23 @@ export const isNothing = (value: unknown): boolean => {
   return false;
 };
 
+/**
+ * Return the second argument if the first one is empty.
+ * You can pass a function as the second argument, it will be called if the first argument is empty.
+ */
 export const otherwise = <T = unknown>(value: T, fallback: unknown): T => {
   if (isNothing(value)) {
     return typeof fallback === "function" ? fallback() : fallback;
   }
   return value;
 };
+
+/**
+ * Casts a value to a type unsafely.
+ * @param value
+ * @returns
+ */
+export const unsafeCoerce = <T>(value: unknown): T => value as unknown as T;
 
 /**
  * Same as isNothing but also returns true for 0.
