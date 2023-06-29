ade62e5da64292a08f0f00fca2b9130dab43dac0
rezonmain <adiazgomez101@gmail.com>
2023-06-28 21:59:14 -0700

    feat(jobRouter): add job api route

---
+++ src/services/api/routes/job.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+const jobRouter = Router();
+
+/**
+ * Get all the registered jobs
+ * @route GET /run
+ * @queryparam ...
+ * @paginated
+ */
+jobRouter.get("/", async (req, res) => {
+  const jobs = req.ctx.scheduler.jobs;
+  res.json(
+    jobs.map((j) => ({
+      name: j.id,
+      status: j.getStatus(),
+    }))
+  );
+});
