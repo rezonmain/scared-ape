fec845ef5d38bff3d1ebc30492db253948f90d1b
rezonmain <adiazgomez101@gmail.com>
2023-07-12 22:53:00 -0700

    feat(authMiddleware): add revoked jwt as condition

--- src/services/api/auth.middleware.ts
+++ src/services/api/auth.middleware.ts
@@ -1,23 +1,34 @@
 import type { NextFunction, request, response } from "express";
 import { isNothingOrZero, parseCookie } from "../../utils/ez.js";
+import { Logger } from "../../utils/Logger.js";
 
-const authenticated = (
+const authenticated = async (
   req: typeof request,
   res: typeof response,
   next: NextFunction
 ) => {
-  const cookies = parseCookie(req.headers.cookie);
-  const fgp = cookies?.["__Secure-fgp"] ?? null;
-  const jwt = cookies?.["__Secure-jwt"] ?? null;
+  try {
+    if (!req.headers.cookie) return res.status(401).send("Unauthenticated");
 
-  if (isNothingOrZero(fgp) || isNothingOrZero(jwt)) {
-    return res.status(401).send("Unauthenticated");
-  }
+    const cookies = parseCookie(req.headers.cookie);
+    const fgp = cookies?.["__Secure-fgp"] ?? null;
+    const jwt = cookies?.["__Secure-jwt"] ?? null;
 
-  if (req.ctx.auth.verifyJWT({ jwt, fgp })) {
-    next();
-  } else {
-    return res.status(403).send("Forbidden");
+    if (isNothingOrZero(fgp) || isNothingOrZero(jwt)) {
+      return res.status(401).send("Unauthenticated");
+    }
+
+    if (
+      req.ctx.auth.verifyJWT({ jwt, fgp }) &&
+      !(await req.ctx.auth.isRevokedJWT({ jwt }))
+    ) {
+      next();
+    } else {
+      return res.status(403).send("Forbidden");
+    }
+  } catch (error) {
+    Logger.error(error);
+    return res.status(500).send("Internal Server Error");
   }
 };
 
