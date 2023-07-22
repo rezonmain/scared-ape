32adff5837c094fde05b7c1821f7834abf5af966
rezonmain <adiazgomez101@gmail.com>
2023-07-22 14:18:55 -0700

    feat(app): scrape on boot, pause notifier

--- src/cli/boot.ts
+++ src/cli/boot.ts
@@ -3,6 +3,6 @@ import { Booter } from "../utils/Booter.js";
 
 (async () => {
   const booter = new Booter();
-  await booter.boot();
+  await booter.boot({ scrapeOnBoot: true });
   process.exit(0);
 })();
