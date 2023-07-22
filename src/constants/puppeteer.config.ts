21c4ea2ac1703a715da245993db62a1d20fe2b80
rezonmain <adiazgomez101@gmail.com>
2023-07-22 14:31:21 -0700

    feat(scrapers): puppeteer args

--- src/constants/puppeteer.config.ts
+++ src/constants/puppeteer.config.ts
@@ -2,10 +2,9 @@ import type { PuppeteerLaunchOptions } from "puppeteer";
 
 const common: PuppeteerLaunchOptions = {
   headless: "new",
+  args: process.env.NODE_ENV === "prod" ? ["--no-sandbox"] : undefined,
   executablePath:
-    process.env.NODE_ENV === "production"
-      ? "/usr/bin/google-chrome"
-      : undefined,
+    process.env.NODE_ENV === "prod" ? "/usr/bin/google-chrome" : undefined,
 };
 
 export { common };
