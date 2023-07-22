7d4a4de75441cc3347a9b8fdcae7a56b5ca83b65
rezonmain <adiazgomez101@gmail.com>
2023-07-22 02:18:55 -0700

    feat(cors): moved allowed origins to config

--- src/services/api/cors.middleware.ts
+++ src/services/api/cors.middleware.ts
@@ -1,12 +1,12 @@
 import type { NextFunction, request, response } from "express";
-import allowedOrigins from "./allowedOrigins.js";
+import c from "config";
 
 const cors = async (
   req: typeof request,
   res: typeof response,
   next: NextFunction
 ) => {
-  res.setHeader("Access-Control-Allow-Origin", allowedOrigins);
+  res.setHeader("Access-Control-Allow-Origin", c.get("allowedOrigins"));
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 };
