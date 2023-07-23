a9d728c13af06b6ad13be0ca59999e094cfad75b
rezonmain <adiazgomez101@gmail.com>
2023-07-22 21:15:07 -0700

    feat(package): add is-docker

--- src/constants/puppeteer.config.ts
+++ src/constants/puppeteer.config.ts
@@ -1,10 +1,10 @@
 import type { PuppeteerLaunchOptions } from "puppeteer";
+import isDocker from "is-docker";
 
 const common: PuppeteerLaunchOptions = {
   headless: "new",
-  args: process.env.NODE_ENV === "prod" ? ["--no-sandbox"] : undefined,
-  executablePath:
-    process.env.NODE_ENV === "prod" ? "/usr/bin/google-chrome" : undefined,
+  args: isDocker() ? ["--no-sandbox"] : undefined,
+  executablePath: isDocker() ? "/usr/bin/google-chrome" : undefined,
 };
 
 export { common };
