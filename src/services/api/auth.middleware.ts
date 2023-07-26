0d98f6d7e1e87e0e282f0d4e3a70cb72ade2a747
rezonmain <adiazgomez101@gmail.com>
2023-07-25 22:59:50 -0700

    feat(authMiddleware): add asPyro middleware

--- src/services/api/auth.middleware.ts
+++ src/services/api/auth.middleware.ts
@@ -32,4 +32,35 @@ const authenticated = async (
   }
 };
 
-export { authenticated };
+const asPyro = async (
+  req: typeof request,
+  res: typeof response,
+  next: NextFunction
+) => {
+  try {
+    const cookies = parseCookie(req.headers.cookie);
+    const jwt = cookies?.["__Secure-jwt"] ?? null;
+
+    if (isNothingOrZero(jwt)) {
+      return res.status(401).send("Unauthenticated");
+    }
+
+    const decodedJWT = req.ctx.auth.decodeJWT(jwt);
+    const user = await req.ctx.db.getUserByCuid(decodedJWT?.cuid ?? "");
+
+    if (isNothingOrZero(user)) {
+      return res.status(401).send("Unauthenticated");
+    }
+
+    if (user.role !== "pyro") {
+      return res.status(403).send("Forbidden");
+    }
+
+    next();
+  } catch (error) {
+    Logger.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export { authenticated, asPyro };
