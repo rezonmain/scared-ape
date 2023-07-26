92997644d644d80c16eab727dc08e9e7ad4d822c
rezonmain <adiazgomez101@gmail.com>
2023-07-25 18:13:28 -0700

    feat(middleware): add rate limiter middleware

---
+++ src/services/api/limiter.middleware.ts
@@ -0,0 +1,21 @@
+import rateLimit from "express-rate-limit";
+import type { Options } from "express-rate-limit";
+
+const sharedConfig: Partial<Options> = {
+  legacyHeaders: false,
+  standardHeaders: true,
+  message: { error: "Too many requests" },
+};
+const generalLimiter = rateLimit({
+  ...sharedConfig,
+  windowMs: 30 * 60 * 1000, // 1 hour window
+  max: 100,
+});
+
+const requestAccessLimiter = rateLimit({
+  ...sharedConfig,
+  windowMs: 30 * 60 * 1000, // 30 minutes
+  max: 2,
+});
+
+export { generalLimiter, requestAccessLimiter };
